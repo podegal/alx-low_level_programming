7308b4a7aac0f1a52d8ef1bbee6965603b33e193
podegal <emmaomene@yahoo.com>
2023-02-17 15:54:40 -0800

    determine whether the number stored in the variable n is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,17 +5,17 @@
 /**
 * main - print if the number is postive, zero, or negative
 *
-* Description: using the main function by Adooz
-* this program prints "Programming is positive, zero, or negative
+* Description: using the main function, this program prints "the number, followed by if it is  positive, zero, or negative"
 * Return: 0
 */
 int main(void)
 {
+
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-  /* your code goes there */
+  
 if (n > 0)
 {
 	printf("%i is positive\n", n);
