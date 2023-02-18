93bf43980004bd77faa45e6f931a1cfffd1e062c
podegal <emmaomene@yahoo.com>
2023-02-17 18:10:04 -0800

    program that prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
- * main - program prints all possible combinations of single-digit numbers putting space and comma.
- *
+ * main - printing numbers from 0-9 with commas and space between them
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
  * Return: 0
  */
 int main(void)
 {
-int b;
+	int q;
 
-for (b = 18; b <= 27; c++)
-{
-putchar(b);
-if (b != 27)
-{
- putchar(',');
- putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	for (q = 38; q <= 47; c++)
+	{
+		putchar(q);
+		if (c != 47)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
