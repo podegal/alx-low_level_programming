846b299afc130ce87b060f1b02b05cb7f8b4bca9
podegal <emmaomene@yahoo.com>
2023-02-23 08:59:05 -0800

    Comment out (don’t delete it!) the part of the code that is causing the output to go into an infinite loop.

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/* while (i < 10) 
+	{ 
+	      putchar(i);
+	}*/
+
+	//i++ is required after the putchar(i) curly braces to increment the value of i and avoid an infinite loop 
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
