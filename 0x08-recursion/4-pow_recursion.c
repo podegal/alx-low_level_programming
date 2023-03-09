69b98ae41b16f199a33e69a4ff4dd2b2d9a1caf6
podegal <emmaomene@yahoo.com>
2023-03-08 19:17:49 -0800

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _pow_recursion - A function that returns the value of x raised to the power of y.
  * @x: the number to multiply
@@ -7,14 +6,13 @@
  * Return: returns 1 if y equals 0
  * If y is less than 0, the function returns -1.
  */
-
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-
-	if (y == 0)
+	else if (y == 0)
 		return (1);
-
-	return (x * _pow_recursion(x, y - 1));
+	else
+		return (x * _pow_recursion(x, y - 1));
+return (0);
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,10 @@
 #include "main.h"
-int isPrime(int n);
+int isPrime(int num, int i);
 
 /**
- * is_prime_number - A function that returns 1 if the input integer is a prime number, otherwise return 0.
- * @n: input integer to check if prime
+ * is_prime_number - A function that returns 1
+ * if the input integer is a prime number.
+ * @n: input number
  * Return: returns 1 if the input is a prime number,
  * otherwise return 0.
  */
@@ -22,7 +23,7 @@ int is_prime_number(int n)
 /**
  * isPrime - checks if the input modulus i == 0
  * if not calls the recursive function
- * @num: input integer
+ * @num: input number
  * @i: iterator
  * Return: returns 1 if the input is a prime number, else 0
  */
