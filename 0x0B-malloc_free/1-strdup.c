94b3666a4a63fcbca3c5bba8fc87ba9099e6c831
podegal <emmaomene@yahoo.com>
2023-03-13 16:37:39 -0700

    a function that concatenates two strings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: string to duplicate
+ * Return: pointer to duplicated string in allocated memory
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate_str;
+	int i, len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	
+	duplicate_str = malloc(sizeof(char) * (len + 1));
+	if (duplicate_str == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (i < len)
+	{
+		*(duplicate_str + i) = *(str + i);
+		i++;
+	}
+	return (duplicate_str);
+}
