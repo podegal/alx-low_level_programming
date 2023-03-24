a7e49f850fc757565f04ce1b8906d734d8b36ac5
podegal <emmaomene@yahoo.com>
2023-03-23 17:02:07 -0700

    advanced function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,51 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-void printOpCode(char *func, int size);
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
- * main - prints the opcodes of the first
- * n bytes of the main function
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 if successful, 1 if arguments not equal to 1
- * , 2 if the argument not positive
+ * main- Entry point
+ * @argc: the number of parameters.
+ * @argv: the parameeters in the case the number of bytes.
+ * Return: 0 in succes
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	int i, n;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) < 0)
+	n = atoi(argv[1]);
+
+	if (n < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
-	char *begin = (char *)main;
-
-	printOpCode(begin, atoi(argv[1]));
-
-	return (0);
-}
-
-/**
- * printOpCode - prints the opcodes of the first n bytes of a function
- * @func: pointer to a function
- * @size: number of bytes to print
- */
-
-
-void printOpCode(char *func, int size)
-{
-	int i = 0;
-
-	while (i < size)
+	for (i = 0; i < n; i++)
 	{
-		printf("%02x ", func[i] & 0xff);
-		i++;
+		printf("%02hhx", *((char *)main + i));
+
+		if (i < n - 1)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
 	}
-	putchar('\n');
+	return (0);
 }
