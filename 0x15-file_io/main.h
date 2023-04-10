a9b566035297bd480acd03d379ae9bf79fe2a409
podegal <emmaomene@yahoo.com>
2023-04-10 16:03:14 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text);
 int append_text_to_file(const char *filename, char *text);
+void error_file(int file_from, int file_to, char *argv[]);
 int _putchar(char c);
 
 #endif
