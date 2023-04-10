8b9ed204f48b1cd8cf7402f62b7875f84345da36
podegal <emmaomene@yahoo.com>
2023-04-10 15:31:37 -0700

    Create a function that creates a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text);
 int append_text_to_file(const char *filename, char *text);
-int _putchar(char c)
+int _putchar(char c);
 
 #endif
